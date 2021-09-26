0d015d47f25bacca0ee17a0ec197016d7a293b48
Dylan Elliott <dylan.elliott@gmail.com>
2021-09-26 13:59:41 +1000

    Initial commit

---
+++ Turtles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Turtles
+//
+//  Created by Dylan Elliott on 26/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Turtles/TurtlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurtlesApp.swift
+//  Turtles
+//
+//  Created by Dylan Elliott on 26/9/21.
+//
+
+import SwiftUI
+
+@main
+struct TurtlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
